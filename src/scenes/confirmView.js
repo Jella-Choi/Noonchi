540573e275dd604b1db32385072de1f2d3e00b6d
Choi Jimin <ewqazxc319@gmail.com>
2020-09-26 03:21:15 +0900

    design : confirmView

--- src/scenes/confirmView.js
+++ src/scenes/confirmView.js
@@ -1,12 +1,14 @@
 import 'react-native-gesture-handler';
 import React, {Component, useState} from 'react';
 import {StyleSheet, View, Text, Button, CheckBox} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+//import {CheckBox} from 'react-native-elements';
 
 class confirmView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isSelected: false,
+      checked: false,
       setSelection: false,
     };
   }
@@ -14,9 +16,8 @@ class confirmView extends Component {
   render() {
     return (
       <View style={styles.screen}>
-        <Text style={styles.guide_title}>
-          집합시설 출입을 위한 QR 체크인을 시작합니다.
-        </Text>
+        <Text style={styles.guide_title}>집합시설 출입을 위한</Text>
+        <Text style={styles.guide_title}> QR 체크인을 시작합니다.</Text>
         <Text style={styles.guide}>
           감염병예방법 제 49조, 제 76조의 2에 따른 감염병 예방 및 감염 전파의
           차단을 위해 개인 정보 보호법 제 15조 및 제 17조에 따라 개인정보의
@@ -24,21 +25,75 @@ class confirmView extends Component {
         </Text>
 
         <View style={styles.chkbox}>
-          <CheckBox
-            value={this.state.isSelected}
-            onValueChange={this.state.setSelection}
-            style={styles.checkbox}
+          <View style={styles.chkitem}>
+            <CheckBox
+              value={this.state.checked}
+              onValueChange={(value) => this.setState({checked: value})}
+            />
+            <Text style={styles.minitop}>전체 동의</Text>
+          </View>
+          <View
+            style={{
+              borderBottomColor: 'black',
+              borderBottomWidth: 1,
+              margin: 5,
+              borderBottomColor: '#a49da8',
+            }}
           />
-          <Text style={styles}>[필수] 개인정보 수집 및 이용</Text>
 
-          <Text>[필수] 개인정보 제3자 제공</Text>
-          <Text>[필수] 보건복지부 개인정보 수집 및 이용</Text>
-          <Text>[필수] 보건복지부 개인정도 제3자 제공</Text>
+          <View style={styles.chkitem}>
+            <CheckBox
+              value={this.state.checked}
+              onValueChange={(value) => this.setState({checked: value})}
+            />
+            <Text style={styles.mini}>[필수] 개인정보 수집 및 이용</Text>
+          </View>
+
+          <View style={styles.chkitem}>
+            <CheckBox
+              value={this.state.checked}
+              onValueChange={(value) => this.setState({checked: value})}
+              /*
+              value={this.state.checked}
+              onValueChange={() =>
+                this.setState({checked: !this.state.setSelection})
+              }
+              */
+            />
+            <Text style={styles.mini}>[필수] 개인정보 제3자 제공</Text>
+          </View>
+
+          <View style={styles.chkitem}>
+            <CheckBox
+              value={this.state.checked}
+              onValueChange={(value) => this.setState({checked: value})}
+            />
+            <Text style={styles.mini}>
+              [필수] 보건복지부 개인정보 수집 및 이용
+            </Text>
+          </View>
+
+          <View style={styles.chkitem}>
+            <CheckBox
+              value={this.state.checked}
+              onValueChange={(value) => this.setState({checked: value})}
+            />
+            <Text style={styles.mini}>
+              [필수] 보건복지부 개인정도 제3자 제공
+            </Text>
+          </View>
         </View>
-        <Button
-          title="Go to msg"
-          onPress={() => this.props.navigation.navigate('Msg')}
-        />
+        <TouchableOpacity
+          style={styles.msgBtn}
+          onPress={() => this.props.navigation.navigate('Msg')}>
+          <Text
+            style={{
+              color: 'white',
+              fontWeight: 'bold',
+            }}>
+            동의
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -49,7 +104,8 @@ export default confirmView;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    alignItems: 'center',
+    marginTop: 60,
+    //alignItems: 'center',
     //justifyContent: 'center',
   },
 
@@ -58,23 +114,50 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#6e736f',
     marginRight: 'auto',
-    marginTop: 20,
+    marginTop: 0,
     marginLeft: 7,
-    marginBottom: 'auto',
+    //marginBottom: 'auto',
   },
   guide: {
-    paddingTop: 0,
-    fontSize: 16,
+    paddingTop: 10,
+    fontSize: 14,
+    color: '#a2a0a3',
     marginLeft: 10,
-    marginTop: 0,
-    marginBottom: 'auto',
+    marginTop: 8,
+    //marginBottom: 'auto',
     marginRight: 7,
   },
 
   chkbox: {
-    backgroundColor: 'purple',
-    opacity: 0.3,
-    flex: 0.3,
+    marginLeft: 5,
+    marginTop: 60,
+    padding: 10,
+    //flex: 1,
+    width: 400,
+    height: 220,
+    backgroundColor: '#dcd9de',
+    opacity: 0.7,
     borderRadius: 10,
+  },
+  chkitem: {
+    marginTop: 5,
+    marginLeft: 5,
+    flexDirection: 'row',
+  },
+  minitop: {
+    fontSize: 17,
+  },
+  mini: {
+    fontSize: 12,
+    marginTop: 5,
+  },
+  msgBtn: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 40,
+    marginTop: 20,
+    margin: 10,
+    borderRadius: 10,
+    backgroundColor: 'purple',
   },
 });
